5af5829e29d05c7b8fad3a61301aba819479284f
greenish0902 <greenish0902@gmail.com>
2022-07-14 15:48:47 +0900

    chore: prettier 설정 적용

--- src/components/wrappers/Table.tsx
+++ src/components/wrappers/Table.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Table = () => {
-  return <div>Table</div>;
-};
+const Table = styled.table`
+  margin: auto;
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  border-top: 3px solid #178;
+  border-collapse: collapse;
+  th {
+    color: #168;
+    background: #f0f6f9;
+    padding: 10px;
+    border: 1px solid #ddd;
+    &:first-child {
+      border-left: 0;
+    }
+    &:last-child {
+      border-right: 0;
+    }
+  }
+  td {
+    padding: 10px;
+    border: 1px solid #ddd;
+    &:first-child {
+      border-left: 0;
+    }
+    &:last-child {
+      border-right: 0;
+    }
+  }
+`;
 
 export default Table;

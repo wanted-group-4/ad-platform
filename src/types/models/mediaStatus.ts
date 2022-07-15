8b2295d5fe32df99912dffaf0ad61a39f91e17ee
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-07-15 11:46:08 +0900

    feat: 타입스크립트 타입 글로벌 선언

---
+++ src/types/models/mediaStatus.ts
@@ -0,0 +1,12 @@
+export interface IMediaStatus {
+  channel: string;
+  data: string;
+  imp: number;
+  click: number;
+  cost: number;
+  convValue: number;
+  crt: number;
+  cpc: number;
+  cpa: number;
+  roas: number;
+}

73360726a602655252461d897bcaf30b6e009bf0
mirim <alfla6528@naver.com>
2022-07-15 13:55:56 +0900

    feat: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,42 @@
+const size = {
+  mobile: '(max-width:480px)',
+};
+
+const color = {
+  black: '#4A4A4A',
+  grey_01: 'A3A3A3',
+  greay_02: '737678',
+};
+
+const background = {
+  main: '#f5f7fb',
+  sidebar: '#323B4D',
+  table: '#F5F5F5',
+  scroll: '#D9D9D9',
+};
+
+const hover = {
+  menu: '#202736',
+  button: '#347CEA',
+  box: '#63AAF8',
+};
+
+const border = {
+  primary: '1px solid #BABDBF',
+};
+
+const boxShadow = {
+  board: '2px 2px 10px rgba(0, 0, 0, 0.2);',
+  modal: '10px 10px 30px rgba(0, 0, 0, 0.4)',
+};
+
+const theme = {
+  size,
+  color,
+  background,
+  border,
+  hover,
+  boxShadow,
+};
+
+export default theme;

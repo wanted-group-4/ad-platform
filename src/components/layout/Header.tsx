6d573b1a210c1eb7fc7e0631792b0b9cae28436a
mirim <alfla6528@naver.com>
2022-07-15 13:58:51 +0900

    feat: Header 컴포넌트 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <IconContainer>
+        <AccountCircleIcon />
+        <NotificationsIcon />
+        <SettingsIcon />
+      </IconContainer>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
+
+const HeaderContainer = styled.header`
+  grid-area: header;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 40px;
+  width: 100%;
+  height: 70px;
+  box-shadow: ${({theme}) => theme.boxShadow.board};
+`;
+const IconContainer = styled.div`
+  display: flex;
+  right: 0;
+  width: 150px;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+`;

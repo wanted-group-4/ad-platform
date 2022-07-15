c44ecc5f1859c7f068b3ab23a620663c5d2baca1
park_so_young <80194405+soyoung931014@users.noreply.github.com>
2022-07-15 16:01:29 +0900

    feat: Board page 레이아웃 세팅 (#9)

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,7 +1,88 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Board() {
-  return <div>Board</div>;
+  return (
+    <BoardContainer>
+      <DashBoard>
+        <Title>대시보드</Title>
+        <DateSelection>년/월/일</DateSelection>
+      </DashBoard>
+      <IntegrationAd>
+        <Title>통합 광고 현황</Title>
+        <DataBox>
+          <DataCard>데이터 카드 컴포넌트</DataCard>
+          <GraphChart>차트 컴포넌트</GraphChart>
+        </DataBox>
+      </IntegrationAd>
+      <CurrentStateOfAd>
+        <Title>매체 현황</Title>
+        <DataBox>
+          <BarChart>바 차트 컴포넌트</BarChart>
+          <Diagram> 표 컴포넌트</Diagram>
+        </DataBox>
+      </CurrentStateOfAd>
+    </BoardContainer>
+  );
 }
 
 export default Board;
+
+const BoardContainer = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  height: 170vh;
+`;
+const DashBoard = styled.div`
+  border: solid red 2px;
+  display: flex;
+  height: 5.5vh;
+  display: flex;
+  flex: 0 1 auto;
+`;
+const IntegrationAd = styled.div`
+  border: solid red 2px;
+  height: 40vh;
+  padding: 2rem;
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+`;
+const CurrentStateOfAd = styled.div`
+  border: solid red 2px;
+  height: 40vh;
+  padding: 2rem;
+  flex: 1 1 auto;
+`;
+const Title = styled.div`
+  width: 15%;
+  height: 1rem;
+`;
+const DateSelection = styled.div`
+  flex: 1 0 auto;
+`;
+const DataBox = styled.div`
+  border: solid 2px black;
+  display: flex;
+  flex-direction: column;
+  height: 70vh;
+  flex: 1 1 auto;
+`;
+const DataCard = styled.div`
+  border: dotted 2px red;
+  flex: 0.5 0 auto;
+`;
+const GraphChart = styled.div`
+  border: dotted 2px red;
+  flex: 1 0 auto;
+`;
+const BarChart = styled.div`
+  border: dotted 2px red;
+  flex: 1 0 auto;
+`;
+
+const Diagram = styled.div`
+  border: dotted 2px red;
+  flex: 0.9 0 auto;
+`;

42730f3e3dfc467f4cdf4333e2926957f2bd033b
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-07-16 22:30:13 +0900

    feat: chart 스타일 수정 & dropdown 레이아웃 잡아놓기

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -2,11 +2,9 @@ import React, {useEffect} from 'react';
 import axios from 'axios';
 import * as Charts from 'recharts';
 import {format, parseISO} from 'date-fns';
+import styled from 'styled-components';
 
 export default function LineChart() {
-<<<<<<< HEAD
-  return <div>LineChart</div>;
-=======
   const [dbData, setDbData] = React.useState([]);
 
   useEffect(() => {
@@ -32,44 +30,75 @@ export default function LineChart() {
           });
           setDbData(newFormat);
         })
-        .catch(error => console.log(error));
+        .catch((error: Error) => console.log(error));
     }
     getData();
   }, []);
 
   return (
-    <Charts.ResponsiveContainer width="100%" height="100%">
-      <Charts.LineChart
-        width={500}
-        height={300}
-        data={dbData}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <Charts.CartesianGrid vertical={false} />
-        <Charts.XAxis dataKey="DATE" padding={{left: 50, right: 50}} />
-        <Charts.YAxis dataKey="ROAS" />
-        <Charts.Tooltip />
-        <Charts.Line
-          type="linear"
-          dataKey="CLICK"
-          stroke="#2d23ed"
-          activeDot={{r: 5}}
-          dot={false}
-        />
-        <Charts.Line
-          type="linear"
-          dataKey="ROAS"
-          stroke="#19f46d"
-          activeDot={{r: 5}}
-          dot={false}
-        />
-      </Charts.LineChart>
-    </Charts.ResponsiveContainer>
+    <Container>
+      <DropwDownContainer>
+        <DropwDownButton>Select</DropwDownButton>
+        <DropwDownButton>Select</DropwDownButton>
+      </DropwDownContainer>
+
+      <ChartBackground>
+        <Charts.ResponsiveContainer width="100%" height="100%">
+          <Charts.LineChart width={1050} height={350} data={dbData}>
+            <Charts.CartesianGrid vertical={false} />
+            <Charts.XAxis dataKey="DATE" padding={{left: 50, right: 50}} />
+            <Charts.YAxis dataKey="ROAS" padding={{top: 50}} />
+            <Charts.Tooltip />
+            <Charts.Line
+              type="linear"
+              dataKey="CLICK"
+              stroke="#8884d8"
+              activeDot={{r: 5}}
+              dot={false}
+            />
+            <Charts.Line
+              type="linear"
+              dataKey="ROAS"
+              stroke="#82ca9d"
+              activeDot={{r: 5}}
+              dot={false}
+            />
+          </Charts.LineChart>
+        </Charts.ResponsiveContainer>
+      </ChartBackground>
+    </Container>
   );
->>>>>>> eaa8d85 (feat: recharts mockdata 와 연결 & date format 변경)
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1050px;
+  height: 350px;
+  background: #ffffff;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 24px 25px;
+  gap: 26.96px;
+`;
+
+const ChartBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #d9d9d9;
+`;
+
+const DropwDownContainer = styled.div`
+  display: flex;
+  gap: 14.23px;
+`;
+
+const DropwDownButton = styled.div`
+  width: 119.87px;
+  height: 36.04px;
+  background: #ffffff;
+  border: 1px solid #b4b4b4;
+  border-radius: 10px;
+  text-align: center;
+  line-height: 36.04px;
+`;

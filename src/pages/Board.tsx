5a93fb93a32527ffb5fddd23373a1ba28490334c
greenish0902 <greenish0902@gmail.com>
2022-07-18 16:44:29 +0900

    fix: Board 페이지 콘솔 삭제

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -5,14 +5,6 @@ import styled from 'styled-components';
 import {reportState, channelState} from '../api/selectors';
 
 export default function Board() {
-  const date = '2022-02-01';
-
-  const weeklyReports = useRecoilValue(reportState(new Date(date)));
-  console.log('weeklyReports', weeklyReports);
-
-  const weeklyChannels = useRecoilValue(channelState(new Date(date)));
-  console.log('weeklyChannels', weeklyChannels);
-
   return (
     <BoardContainer>
       <DashBoard>

8f6c84bfc3eb773bd49a9fc8a03ac6a510d2bf1c
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-07-18 12:45:36 +0900

    feat: Line chart 타입 정의 & y축 두 데이터 최대값 비교후 적용 기능

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,33 +1,62 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import * as Charts from 'recharts';
 import {format, parseISO} from 'date-fns';
 import styled from 'styled-components';
+import {IDailyAdStatus} from '../../types/models/advertise';
+
+type AdStatusKey =
+  | 'roas'
+  | 'click'
+  | 'cost'
+  | 'conv'
+  | 'convValue'
+  | 'ctr'
+  | 'cvr'
+  | 'cpc'
+  | 'cpa'
+  | 'roas';
 
 export default function LineChart() {
-  const [dbData, setDbData] = React.useState([]);
+  const [dbData, setDbData] = useState([]);
+  const [dataKey1, setDataKey] = useState<AdStatusKey>('roas');
+  const [dataKey2, setDataKey2] = useState<AdStatusKey>('click');
+
+  function getLinebyComparingeMax(line1: AdStatusKey, line2: AdStatusKey) {
+    const line1Data = dbData
+      .map(dailyAdStatus => dailyAdStatus[line1])
+      .sort((start, end) => end - start);
+
+    const line2Data = dbData
+      .map(dailyAdStatus => dailyAdStatus[line2])
+      .sort((start, end) => end - start);
+
+    if (line1Data[0] < line2Data[0]) return line2;
+    return line1;
+  }
 
   useEffect(() => {
     async function getData() {
       await axios
         .get('/db.json')
         .then(response => {
-          const newFormat = response.data.report.daily.map((data: any) => {
-            //
-            return {
-              IMP: data.imp,
-              CLICK: data.click,
-              COST: data.cost,
-              CONV: data.conv,
-              CONVVALUE: data.convValue,
-              CTR: data.ctr,
-              CVR: data.cvr,
-              CPC: data.cpc,
-              CPA: data.cpa,
-              ROAS: data.roas,
-              DATE: format(parseISO(data.date), 'MM월 dd일'),
-            };
-          });
+          const newFormat = response.data.report.daily.map(
+            (data: IDailyAdStatus) => {
+              return {
+                imp: data.imp,
+                click: data.click,
+                cost: data.cost,
+                conv: data.conv,
+                convValue: data.convValue,
+                ctr: data.ctr,
+                cvr: data.cvr,
+                cpc: data.cpc,
+                cpa: data.cpa,
+                roas: data.roas,
+                date: format(parseISO(data.date), 'MM월 dd일'),
+              };
+            },
+          );
           setDbData(newFormat);
         })
         .catch((error: Error) => console.log(error));
@@ -37,35 +66,36 @@ export default function LineChart() {
 
   return (
     <Container>
-      <DropwDownContainer>
-        <DropwDownButton>Select</DropwDownButton>
-        <DropwDownButton>Select</DropwDownButton>
-      </DropwDownContainer>
-
-      <ChartBackground>
-        <Charts.ResponsiveContainer width="100%" height="100%">
-          <Charts.LineChart width={1050} height={350} data={dbData}>
-            <Charts.CartesianGrid vertical={false} />
-            <Charts.XAxis dataKey="DATE" padding={{left: 50, right: 50}} />
-            <Charts.YAxis dataKey="ROAS" padding={{top: 50}} />
-            <Charts.Tooltip />
-            <Charts.Line
-              type="linear"
-              dataKey="CLICK"
-              stroke="#8884d8"
-              activeDot={{r: 5}}
-              dot={false}
-            />
-            <Charts.Line
-              type="linear"
-              dataKey="ROAS"
-              stroke="#82ca9d"
-              activeDot={{r: 5}}
-              dot={false}
-            />
-          </Charts.LineChart>
-        </Charts.ResponsiveContainer>
-      </ChartBackground>
+      <Charts.ResponsiveContainer width="100%" height="100%">
+        <Charts.LineChart
+          margin={{left: 20}}
+          width={1050}
+          height={350}
+          data={dbData}
+        >
+          <Charts.CartesianGrid vertical={false} />
+          <Charts.XAxis dataKey="date" padding={{left: 50, right: 50}} />
+          <Charts.YAxis
+            dataKey={getLinebyComparingeMax(dataKey1, dataKey2)}
+            padding={{top: 50}}
+          />
+          <Charts.Tooltip />
+          <Charts.Line
+            type="monotone"
+            dataKey={dataKey1}
+            stroke="#8884d8"
+            activeDot={{r: 5}}
+            dot={false}
+          />
+          <Charts.Line
+            type="monotone"
+            dataKey={dataKey2}
+            stroke="#82ca9d"
+            activeDot={{r: 5}}
+            dot={false}
+          />
+        </Charts.LineChart>
+      </Charts.ResponsiveContainer>
     </Container>
   );
 }
@@ -80,25 +110,4 @@ const Container = styled.div`
   border-radius: 5px;
   padding: 24px 25px;
   gap: 26.96px;
-`;
-
-const ChartBackground = styled.div`
-  width: 100%;
-  height: 100%;
-  background: #d9d9d9;
-`;
-
-const DropwDownContainer = styled.div`
-  display: flex;
-  gap: 14.23px;
-`;
-
-const DropwDownButton = styled.div`
-  width: 119.87px;
-  height: 36.04px;
-  background: #ffffff;
-  border: 1px solid #b4b4b4;
-  border-radius: 10px;
-  text-align: center;
-  line-height: 36.04px;
 `;

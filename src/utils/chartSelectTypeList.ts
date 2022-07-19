45c148314a19a43fcfb4b0ba60f385bbe690bdcd
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-07-19 14:28:11 +0900

    feat: line chart 구현 (#3)
    * feat: recharts mockdata 와 연결 & date format 변경
    
    * feat: chart 스타일 수정 & dropdown 레이아웃 잡아놓기
    
    * feat: Line chart 타입 정의 & y축 두 데이터 최대값 비교후 적용 기능
    
    * feat: 드롭다운 구현 & Line chart에 적용
    
    * feat: dropdwon 두번째 인자로 건내줄 selectList 추가
    
    * remove: 원래 구현했던 dropdown 제거
    
    * refactor: 함수,변수 타입 재정의
    
    * refactor: 드롭다운을 위한 select 리스트 utils폴더로 분리
    
    * design: 차트 컨테이너 고정크기 -> 부모 기준으로 변경
    
    * feat: line chart 셀렉트 roas 옵션 추가
    
    * chore: pulbic에 dbData 임시로 생성 & 데이터구조에 맞게 수정

---
+++ src/utils/chartSelectTypeList.ts
@@ -0,0 +1,14 @@
+export const chartSelectTypeList = [
+  ['imp', 'imp'],
+  ['click', 'click'],
+  ['cost', 'cost'],
+  ['conv', 'conv'],
+  ['convValue', 'convValue'],
+  ['ctr', 'ctr'],
+  ['cvr', 'cvr'],
+  ['cpc', 'cpc'],
+  ['cpa', 'cpa'],
+  ['roas', 'roas'],
+];
+
+export default chartSelectTypeList;

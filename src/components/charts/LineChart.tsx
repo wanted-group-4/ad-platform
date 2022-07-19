88df2763109f4ee5e88257d2b78130881b821c63
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-07-19 14:28:11 +0900

    feat: line chart 구현 (#3)
    * feat: recharts mockdata 와 연결 & date format 변경
    
    * feat: chart 스타일 수정 & dropdown 레이아웃 잡아놓기
    
    * feat: Line chart 타입 정의 & y축 두 데이터 최대값 비교후 적용 기능
    
    * feat: 드롭다운 구현 & Line chart에 적용
    
    * feat: dropdwon 두번째 인자로 건내줄 selectList 추가
    
    * remove: 원래 구현했던 dropdown 제거
    
    * refactor: 함수,변수 타입 재정의
    
    * refactor: 드롭다운을 위한 select 리스트 utils폴더로 분리
    
    * design: 차트 컨테이너 고정크기 -> 부모 기준으로 변경
    
    * feat: line chart 셀렉트 roas 옵션 추가
    
    * chore: pulbic에 dbData 임시로 생성 & 데이터구조에 맞게 수정

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,5 +1,118 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import * as Charts from 'recharts';
+import {format, parseISO} from 'date-fns';
+import styled from '@emotion/styled';
+import DropDown from '@src/components/dropdown/DropDown';
+import {SelectChangeEvent} from '@mui/material';
+import chartSelectTypeList from '@src/utils/chartSelectTypeList';
+import {IDailyAdStatus} from '../../types/models/advertise';
 
 export default function LineChart() {
-  return <div>LineChart</div>;
+  const [dbData, setDbData] = useState([]);
+  const [dataKey1, setDataKey] = useState<string>('roas');
+  const [dataKey2, setDataKey2] = useState<string>('click');
+
+  function getLinebyComparingeMax(line1: string, line2: string) {
+    const line1Data = dbData
+      .map(dailyAdStatus => dailyAdStatus[line1])
+      .sort((start, end) => end - start);
+
+    const line2Data = dbData
+      .map(dailyAdStatus => dailyAdStatus[line2])
+      .sort((start, end) => end - start);
+
+    if (line1Data[0] < line2Data[0]) return line2;
+    return line1;
+  }
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    event.stopPropagation();
+    setDataKey(event?.target.value);
+  };
+
+  const handleChange2 = (event: SelectChangeEvent<string>) => {
+    event.stopPropagation();
+    setDataKey2(event?.target.value);
+  };
+
+  useEffect(() => {
+    async function getData() {
+      await axios
+        .get('/db.json')
+        .then(response => {
+          const newFormat = response.data.daily.map((data: IDailyAdStatus) => {
+            return {
+              ...data,
+              date: format(parseISO(data.date), 'MM월 dd일'),
+            };
+          });
+          setDbData(newFormat);
+        })
+        .catch((error: Error) => console.log(error));
+    }
+    getData();
+  }, []);
+
+  return (
+    <Container>
+      <DropwDownContainer>
+        <DropDown
+          handleChange={handleChange}
+          optionData={chartSelectTypeList}
+        />
+        <DropDown
+          handleChange={handleChange2}
+          optionData={chartSelectTypeList}
+        />
+      </DropwDownContainer>
+      <Charts.ResponsiveContainer width="100%" height="100%">
+        <Charts.LineChart
+          margin={{left: 20}}
+          width={1050}
+          height={350}
+          data={dbData}
+        >
+          <Charts.CartesianGrid vertical={false} />
+          <Charts.XAxis dataKey="date" padding={{left: 50, right: 50}} />
+          <Charts.YAxis
+            dataKey={getLinebyComparingeMax(dataKey1, dataKey2)}
+            padding={{top: 50}}
+          />
+          <Charts.Tooltip />
+          <Charts.Line
+            type="monotone"
+            dataKey={dataKey1}
+            stroke="#8884d8"
+            activeDot={{r: 5}}
+            dot={false}
+          />
+          <Charts.Line
+            type="monotone"
+            dataKey={dataKey2}
+            stroke="#82ca9d"
+            activeDot={{r: 5}}
+            dot={false}
+          />
+        </Charts.LineChart>
+      </Charts.ResponsiveContainer>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 24px 25px;
+  gap: 26.96px;
+`;
+
+const DropwDownContainer = styled.div`
+  display: flex;
+  gap: 14.23px;
+`;

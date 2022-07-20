9683eb11b744337268af2f50c00f966364aa5c9c
park_so_young <80194405+soyoung931014@users.noreply.github.com>
2022-07-20 13:16:00 +0900

    Fix: 파일 경로 수정, 삭제 및 마진추가 (#68)
    * fix: 파일 삭제
    
    * fix: 경로수정
    
    * style: 마진추가

--- src/components/dropdown/DropDown.tsx
+++ src/components/dropdown/DropDown.tsx
@@ -10,18 +10,15 @@ interface DropDownProps {
 export default function DropDown({handleChange, optionData}: DropDownProps) {
   return (
     <FormControl sx={{m: 1, minWidth: 120}} size="small">
-      <Select
-        defaultValue={optionData[0][0]}
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        onChange={handleChange}
-      >
-        {optionData.map(option => (
-          <MenuItem value={option[0]} key={option[0]}>
-            {option[1]}
-          </MenuItem>
-        ))}
-      </Select>
+      {optionData.length > 0 ? (
+        <Select defaultValue={optionData[0][0] || ''} onChange={handleChange}>
+          {optionData.map(option => (
+            <MenuItem value={option[0] || ''} key={option[0]}>
+              {option[1]}
+            </MenuItem>
+          ))}
+        </Select>
+      ) : null}
     </FormControl>
   );
 }

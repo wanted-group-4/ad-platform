f3c5cde323af190f5bbf26d0153c8e8c8d502ee8
mirim <alfla6528@naver.com>
2022-07-20 16:26:35 +0900

    refactor: 컴포넌트로 이동

--- src/utils/adSelectData.ts
+++
@@ -1,8 +0,0 @@
-import {IAds} from '@type/models/management';
-
-const adselectData = (data: IAds[], type: string) => {
-  if (type === 'all') return data;
-  return data.filter(item => item.status === type);
-};
-
-export default adselectData;
--- src/utils/adSelectTypeList.ts
+++
@@ -1,7 +0,0 @@
-const adSelectTypeList = [
-  ['all', '전체광고'],
-  ['active', '진행광고'],
-  ['ended', '종료된광고'],
-];
-
-export default adSelectTypeList;

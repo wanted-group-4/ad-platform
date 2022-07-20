73dee392c9da5ea89c30e3e8c30cbe9acb76001e
park_so_young <80194405+soyoung931014@users.noreply.github.com>
2022-07-20 10:45:55 +0900

    feat: card 수정 및 이벤트 발생시 다른 이벤트 막기 (#66)
    * feat: 이전 날짜 불러오기
    
    * feat: dataCard에  type 연결
    
    * feat: 이벤트 loading시 다른 이벤트  막기
    
    * feat: 이벤트 발생시 다른 이벤트 막기
    
    * feat: 데이터 비교
    
    * feat: card로직수정
    
    * fix: 경로수정

--- src/components/dataCard/card.tsx
+++ src/components/dataCard/card.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import styled from '@emotion/styled';
-// import {IDailyAdStatus} from '@type/models/advertise';
+
+// import {IDailyAdStatus} from '@src/types/models/advertise';
+import {useQueries} from 'react-query';
+import {getReport} from '@src/api/queries';
 
 //  IDailyAdStatus[]
-function Card({info}: any) {
-  // roas, 전환수(conv),
-  function calculate(category: any) {
+function Card({info, type}: any) {
+  const queryResult = useQueries([
+    {
+      queryKey: ['report', type],
+      queryFn: () => getReport(new Date(type)),
+    },
+  ]);
+
+  const {data} = queryResult[0];
+
+  // roas,
+  function calculate(weeklyInfo: any, category: any) {
     const result =
-      info.reduce((acc: number, cur: any) => acc + cur[category], 0) / 7;
+      weeklyInfo.reduce((acc: number, cur: any) => acc + cur[category], 0) / 7;
     return parseInt(String(result), 10);
   }
-  // 전환수(conv), 광고비(cost) 클릭수(click), 노출수(imp), 매출(cost)
-  function calculateSum(category: any) {
-    let result = info.reduce((acc: number, cur: any) => acc + cur[category], 0);
+  // 전환수(conv), 광고비(cost) 클릭수(click), 노출수(imp), 매출(convValue)
+  function calculateSum(weeklyInfo: any, category: any, method?: number) {
+    let result = weeklyInfo.reduce(
+      (acc: number, cur: any) => acc + cur[category],
+      0,
+    );
     result = String(result);
-    if (result.length > 4) {
+    if (result.length > 4 && method === 0) {
       result = result.slice(0, result.length - 4);
+      result = Number(result);
       return `${result}만`;
     }
     return result;
@@ -23,45 +39,55 @@ function Card({info}: any) {
 
   return (
     <Container>
-      {info !== undefined ? (
+      {info && data !== undefined ? (
         <>
           <Div>
             <Title>ROAS</Title>
-            <Content>{calculate('roas')}%</Content>
-            <div>18%</div>
+            <Content>{calculate(info, 'roas')}%</Content>
+            <div>{calculate(data, 'roas') - calculate(info, 'roas')}%</div>
           </Div>
           <Div>
             <Title>광고비</Title>
-            <Content>{calculateSum('cost')} 원</Content>
-            <div>18%</div>
+            <Content>{calculateSum(info, 'cost', 0)} 원</Content>
+            <div>
+              {calculateSum(data, 'cost') - calculateSum(info, 'cost')}원
+            </div>
           </Div>
           <Div>
             <Title>노출 수</Title>
-            <Content>{calculateSum('imp')} 회</Content>
-            <div>18%</div>
+            <Content>{calculateSum(info, 'imp', 0)} 회</Content>
+            <div>{calculateSum(data, 'imp') - calculateSum(info, 'imp')}회</div>
           </Div>
           <Div>
             <Title>클릭 수</Title>
-            <Content>{calculateSum('click')} 회</Content>
-            <div>18%</div>
+            <Content>{calculateSum(info, 'click', 0)} 회</Content>
+            <div>
+              {calculateSum(data, 'click') - calculateSum(info, 'click')}회
+            </div>
           </Div>
           <Div>
             <Title>전환 수</Title>
-            <Content>{calculateSum('conv')}회</Content>
-            <div>18%</div>
+            <Content>{calculateSum(info, 'conv', 0)}회</Content>
+            <div>
+              {calculateSum(data, 'conv') - calculateSum(info, 'conv')}회
+            </div>
           </Div>
           <Div>
             <Title>매출</Title>
-            <Content>{calculateSum('cost')} 원</Content>
-            <div>18%</div>
+            <Content>{calculateSum(info, 'convValue', 0)} 원</Content>
+            <div>
+              {calculateSum(data, 'convValue') -
+                calculateSum(info, 'convValue')}
+              원
+            </div>
           </Div>
         </>
       ) : null}
     </Container>
   );
 }
-
 export default Card;
+
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
